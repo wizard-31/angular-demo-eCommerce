3af028e4aaa594cc68dacc3c2080764e94dc24cc
Subramanian Venkataraman <subvnkt@gmail.com>
2023-10-07 13:29:14 -0400

    Assignment 1: Create 2 components and add styles

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
+  // selector: '.app-servers' #can be used when component is used as a class like in CSS
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
