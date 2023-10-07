98621e3d37494cb0b1451e313d362fb20c50d45e
Subramanian Venkataraman <subvnkt@gmail.com>
2023-10-07 14:00:18 -0400

    Data Binding - 2. Property Binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
@@ -6,6 +6,16 @@ import { Component } from '@angular/core';
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
-export class ServersComponent {
+export class ServersComponent implements OnInit {
+  allowNewServer:boolean = false;
+
+constructor() {
+  setTimeout(() => {
+    this.allowNewServer = true;
+  }, 2000);
+ }
+
+ngOnInit(): void {
+}
 
 }
